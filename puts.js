a8ca4cfe3ec750d7125585a369745c88c1d334bd
Sergey Klimov <sergey.v.klimov@gmail.com>
2012-12-23 04:14:01 -0800

    initial

---
+++ puts.js
@@ -0,0 +1,13 @@
+//     treeify.js
+//     Luke Plaster <notatestuser@gmail.com>
+//     https://github.com/notatestuser/treeify.js
+
+(function() {
+  var treefy = require('treeify');
+  exports = function(obj) {
+    console.log(treefy.as)
+  };
+  
+
+})();
+
